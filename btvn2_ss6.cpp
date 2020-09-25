4990d40165d7fd682ac4c4baa7f1bfd4b580fb0b
0968377398 <viethoangd33@gmail.com>
2020-09-26 00:23:52 +0700

    nop bai tap ve nha

---
+++ btvn2_ss6.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+	int n;
+	int a[100];
+	printf("Nhap n:");
+	scanf("%d",&n);
+	for(int i=0;i<n;i++){
+		printf("Nhap phan tu thu a[%d]: ",i);
+		scanf("%d",&a[i]);
+		}
+	int min=a[0];
+	for(int i=0;i<n;i++){
+		if(min>a[i] && a[i]>0){
+			min=a[i];
+		}
+	}
+	printf("Phan tu duong nho nhat trong mang la: %d",min);
+	return 0;
+	}
---
+++ btvn_ss6.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+	int n;
+	int a[100];
+	printf("Nhap n:");
+	scanf("%d",&n);
+	for(int i=0;i<n;i++){
+		printf("Nhap phan tu a[%d]: ",i);
+		scanf("%d",&a[i]);
+	}
+	for(int i=n-1;i>=0;i--){
+		if(a[i]%2 != 0){
+			printf("Phan tu le cuoi cung cua mang la: %d",a[i]);
+		}
+	}
+}
